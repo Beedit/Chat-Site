e505f9e9571727b7ce1c7dcde177328298fb0ecf
Beedit <notrealemail@beedit.gay>
2023-04-09 22:40:01 +0200

    Remove comment

--- index.js
+++ index.js
@@ -8,14 +8,6 @@ const server = http.createServer(app)
 const { Server } = require("socket.io")
 const io = new Server(server)
 
-/*
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-*/
 
 const port = process.env.PORT
 
