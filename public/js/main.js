703975328236dfaf670bb8ae462c0b8108d1b3aa
Beedit <notrealemail@beedit.gay>
2023-04-10 01:41:46 +0200

    funny looks that dont actually look that good

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,30 @@
 const socket = io();
 const messageBox = document.getElementById("messageBox") 
 
+const createMsg = (msg, toAppend) => {
+  let container = document.createElement("div")
+  let username = document.createElement("div")
+  let time = document.createElement("div")
+  let message = document.createElement("div")
+
+  container.className = "messageContainer"
+  username.className = "username"
+  time.className = "time"
+  message.className = "message"
+
+  let now = new Date();
+
+  message.innerText = msg
+  time.innerText = `${now.getHours()}:${now.getMinutes()}`
+  username.innerText = "username not implemented lmao"
+
+  container.appendChild(username)
+  container.appendChild(time)
+  container.appendChild(message)
+
+  toAppend.append(container)
+  
+}
 
 form.addEventListener('submit', (err) => {
     err.preventDefault();
@@ -12,7 +36,6 @@ form.addEventListener('submit', (err) => {
 
 socket.on("msg", (msg) => {
     // why doesnt it let me do let x = document.createElement("li").textContent = msg; ??? probably some actual reason that i dont know or are not smart enough to realise
-    let x = document.createElement("li")
-    x.textContent = msg;
-    messageBox.appendChild(x)
+    createMsg(msg, messageBox)
+    window.scrollTo(0, document.body.scrollHeight);
 })
