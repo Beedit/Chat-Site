fea11d454ba502db53cac12ec0b0ab1b6ceccdbb
Beedit <notrealemail@beedit.gay>
2023-04-09 19:20:45 +0200

    like a ton of stuff idk

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("a")
+require('dotenv').config()
+const express = require("express")
+const path = require("path")
+const { compileCss } = require("./js/compileCss")
+
+const port = process.env.PORT
+const app = express()
+
+compileCss({ src: path.join(__dirname, 'views/style'), dest: path.join(__dirname, 'public')})
+
+// app stuff
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+
+// Simple logging
+app.use(function(req, res, next){
+    console.log(`Incomming ${req.method} request from ${req.ip} at ${Date.now()}`)
+    next();
+})
+
+// Stuff goes here
+
+app.get("/", (req, res) => {
+    res.render("main")
+})
+
+
+// 404 not found
+app.get("*", (req, res) => {
+    res.render("404")
+})
+
+// Pings a response back to unused stuff
+app.all("*", (req, res) => { res.send(`${req.method} not supported\n`)})
+
+
+// Actually start it!!!
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
