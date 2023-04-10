3e2fd14936b998086f93420dfbbb8a4d5eb89a7c
Beedit <notrealemail@beedit.gay>
2023-04-10 19:40:55 +0200

    minor cleanup

--- index.js
+++ index.js
@@ -58,7 +58,6 @@ io.on("connection", (socket) => {
     })
 
     socket.on("msg", (msg) => {
-        console.log(`${msg.username}: ${msg.msg}`);
         io.emit("msg", {msg: msg.msg, username: msg.username, encrypted: msg.encrypted})
       });
     
